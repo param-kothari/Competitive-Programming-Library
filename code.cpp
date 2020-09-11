65572352be7d67801f8f8d7387576f4e81f317db
param-kothari <41911452+param-kothari@users.noreply.github.com>
2020-09-11 17:30:21 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,30 @@
+/*
+    Author - Param Kothari
+    Ah shit, here we go again
+*/
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define mk make_pair
+
+typedef long long ll;
+typedef long double ld;
+
+void solve(){
+
+    return;
+}
+
+int main(){
+    IOS;
+    int t = 1;
+    // cin >> t;
+    for(int i = 1; i <= t; i++){
+        solve();
+    }
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,35 @@
+/*
+    Author - Param Kothari
+    Ah shit, here we go again
+*/
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define mk make_pair
+
+typedef long long ll;
+typedef long double ld;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int solve1(){
+    
+}
+
+int solve2(){
+    
+}
+
+int main(){
+    IOS;
+    int t = 1000;
+    // cin >> t;
+    for(int i = 0; i < t; i++){
+        
+    }
+    return 0;
+}
